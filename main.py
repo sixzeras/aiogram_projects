8664385a466d8bf78a96b241b53cedbb879fd91f
Daniil Osadchi <daniilosadchi@gmail.com>
2025-04-02 21:03:55 +0300

    echo-bot

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
-a = 2
-b = 4
-c = 8
-d = 10
-print(a * d / b)
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import Command
+from aiogram.types import Message
+
+BOT_TOKEN = '7983523138:AAGyb3InyJzFKasxzTqTtv72X5kpwYpsSNo'
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
+
+@dp.message(Command(commands=["start"]))
+async def process_start_message(msg: Message):
+    await msg.answer('Привет!\nМеня зовут эхо-бот!\nНапиши мне что-то и я отправлю это тебе!')
+
+@dp.message(Command(commands=["help"]))
+async def process_help_message(msg: Message):
+    await msg.answer('Я эхо-бот, отправь мне сообщение и я отправлю тебе его')
+
+@dp.message()
+async def echo_send(msg: Message):
+    await msg.reply(text=msg.text)
+
+if __name__ == '__main__':
+    dp.run_polling(bot)
