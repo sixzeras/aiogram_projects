6651d5dd88f7edff8586cfa4d3b57832e577483f
Daniil Osadchi <daniilosadchi@gmail.com>
2025-04-04 13:56:20 +0300

    третий коммит добавлен универсальный метод для пересылания сообщения

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
-from aiogram.types import ContentType
 from aiogram import F
 import logging
 
@@ -11,62 +10,40 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
+
+@dp.message(Command(commands='start'))
 async def process_start_message(message: types.Message):
     await message.answer('Привет!\nМеня зовут эхо-бот!\nНапиши мне что-то и я отправлю это тебе!')
 
 
+@dp.message(Command(commands='help'))
 async def process_help_message(message: types.Message):
     await message.answer('Я эхо-бот, отправь мне сообщение и я отправлю тебе его')
 
 
-async def echo_photo(message: types.Message):
-    await message.reply_photo(message.photo[0].file_id)
-
-
+@dp.message(F.sticker)
 async def echo_sticker(message: types.Message):
+    print(message.model_dump(exclude_none=True))
     await message.reply_sticker(message.sticker.file_id)
     info = (
         f"ℹ️ Информация о стикере:\n"
         f"ID: {message.sticker.file_id}\n"
         f"Набор: {message.sticker.set_name or 'нет'}\n"
         f"Эмодзи: {message.sticker.emoji or 'нет'}\n"
-        f"Анимированный: {'да' if message.sticker.is_animated else 'нет'}"
     )
     await message.answer(info)
 
-async def echo_video(message: types.Message):
-    await message.reply_video(message.video.file_id)
 
+@dp.message()
+async def send_echo(message: types.message):
+    try:
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        await message.reply(
+            text='Этот тип апдейтов не поддерживается'
+            'методом send.copy' 
+        )
 
-async def echo_video_message(message: types.Message):
-    await message.reply_video_note(message.video_note.file_id)
-
-
-async def echo_document(message: types.Message):
-    await message.reply_document(message.document.file_id)
-
-
-async def echo_voice(message: types.Message):
-    await message.reply_voice(message.voice.file_id)
-
-
-async def echo_geo(message: types.Message):
-    await message.reply_location( latitude=message.location.latitude, longitude=message.location.longitude)
-
-
-async def echo_send(message: types.Message):
-    await message.reply(text=message.text)
-
-dp.message.register(process_start_message, Command(commands='start'))
-dp.message.register(process_help_message, Command(commands='help'))
-dp.message.register(echo_voice, F.voice)
-dp.message.register(echo_photo, F.photo)
-dp.message.register(echo_video, F.video)
-dp.message.register(echo_sticker, F.sticker)
-dp.message.register(echo_document, F.document)
-dp.message.register(echo_video_message, F.video_note)
-dp.message.register(echo_geo, F.location)
-dp.message.register(echo_send)
 
 if __name__ == '__main__':
     dp.run_polling(bot)
